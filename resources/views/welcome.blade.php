e92182e6d5e712a768526eacc7842cf97e516488
sukna-eg <sukna.ama@gmail.com>
2023-10-01 05:25:49 -0700

    fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -158,7 +158,7 @@
           </div>
         </div>
         <div class="row">
-        	<div class="col-md d-flex align-self-stretch ftco-animate">
+        	<div class="col-md d-flex align-self-stretch ftco-animate order-4">
             <div class="media block-6 services text-center d-block">
               <div class="icon justify-content-center align-items-center d-flex" style="background: #212529"><span class="flaticon-pin"></span></div>
               <div class="media-body">
@@ -167,7 +167,7 @@
               </div>
             </div>
           </div>
-          <div class="col-md d-flex align-self-stretch ftco-animate">
+          <div class="col-md d-flex align-self-stretch ftco-animate order-3">
             <div class="media block-6 services text-center d-block mt-lg-5 pt-lg-4">
               <div class="icon justify-content-center align-items-center d-flex" style="background: #212529"><span class="flaticon-detective"></span></div>
               <div class="media-body">
@@ -176,7 +176,7 @@
               </div>
             </div>
           </div>
-          <div class="col-md d-flex align-self-stretch ftco-animate">
+          <div class="col-md d-flex align-self-stretch ftco-animate order-2 order-md-1">
             <div class="media block-6 services text-center d-block">
               <div class="icon justify-content-center align-items-center d-flex" style="background: #212529"><span class="flaticon-house"></span></div>
               <div class="media-body">
@@ -185,7 +185,7 @@
               </div>
             </div>
           </div>
-          <div class="col-md d-flex align-self-stretch ftco-animate">
+          <div class="col-md d-flex align-self-stretch ftco-animate order-1 order-md-2">
             <div class="media block-6 services text-center d-block mt-lg-5 pt-lg-4">
               <div class="icon justify-content-center align-items-center d-flex" style="background: #212529"><span class="flaticon-purse"></span></div>
               <div class="media-body">
@@ -397,16 +397,19 @@
 			<div class="container">
 				<div class="row"  >
 
-                    <div class="col-md-4 d-flex align-self-stretch ftco-animate ">
-                        <div class="media block-6 services text-center d-block">
-                          <div class="icon justify-content-center align-items-center d-flex" style="background: #212529"><span>3</span></div>
+                    <div class="col-md-4 heading-section ftco-animate ">
+                        <h2 class="mb-4" style="text-align: right;font-family: 'Tajawal', sans-serif"> كيف يعمل ؟</h2>
+                        <p style="text-align: right;font-family: 'Tajawal', sans-serif">إليك خطوات إنشاء حسابك كمطوّر عقاري للاستفادة من مميزات التطبيق من إضافة وتعديل العقارات ومتابعة حجوزات العقارات.</p>
+                        <div class="media block-6 services text-center d-block pt-md-5 mt-md-5">
+                          <div class="icon justify-content-center align-items-center d-flex" style="background: #212529"><span>1</span></div>
                           <div class="media-body p-md-3">
-                            <h3 class="heading mb-3" style="font-weight: bold;font-family: 'Tajawal', sans-serif">ابدأ باستقبال الحجوزات</h3>
-                            <p class="mb-5" style="font-family: 'Tajawal', sans-serif">بعد تحويل حسابك إلى مطوّر عقاري يمكنك إضافة عقاراتك بكل سهولة والتعديل علي تفاصيلها مثل السعر والحالة .. إلخ في أي وقت. كما تقوم باستقبال الحجوزات والتأكيد عليها وتعديلها وحذفها متى شئت.</p>
+                            <h3 class="heading mb-3" style="font-weight: bold;font-family: 'Tajawal', sans-serif">إنشاء حساب من خلال التطبيق </h3>
+                            <p class="mb-5" style="font-family: 'Tajawal', sans-serif">قم بتحميل التطبيق من المتجر ومن ثم قم بإنشاء حساب إن لم يكن لديك حساب أو تسجيل الدخول إلي التطبيق.</p>
                             <hr>
                           </div>
                         </div>
                       </div>
+
 
 
 
@@ -422,14 +425,13 @@
                         </div>
                       </div>
 
-                      <div class="col-md-4 heading-section ftco-animate ">
-                        <h2 class="mb-4" style="text-align: right;font-family: 'Tajawal', sans-serif"> كيف يعمل ؟</h2>
-                        <p style="text-align: right;font-family: 'Tajawal', sans-serif">إليك خطوات إنشاء حسابك كمطوّر عقاري للاستفادة من مميزات التطبيق من إضافة وتعديل العقارات ومتابعة حجوزات العقارات.</p>
-                        <div class="media block-6 services text-center d-block pt-md-5 mt-md-5">
-                          <div class="icon justify-content-center align-items-center d-flex" style="background: #212529"><span>1</span></div>
+
+                      <div class="col-md-4 d-flex align-self-stretch ftco-animate ">
+                        <div class="media block-6 services text-center d-block">
+                          <div class="icon justify-content-center align-items-center d-flex" style="background: #212529"><span>3</span></div>
                           <div class="media-body p-md-3">
-                            <h3 class="heading mb-3" style="font-weight: bold;font-family: 'Tajawal', sans-serif">إنشاء حساب من خلال التطبيق </h3>
-                            <p class="mb-5" style="font-family: 'Tajawal', sans-serif">قم بتحميل التطبيق من المتجر ومن ثم قم بإنشاء حساب إن لم يكن لديك حساب أو تسجيل الدخول إلي التطبيق.</p>
+                            <h3 class="heading mb-3" style="font-weight: bold;font-family: 'Tajawal', sans-serif">ابدأ باستقبال الحجوزات</h3>
+                            <p class="mb-5" style="font-family: 'Tajawal', sans-serif">بعد تحويل حسابك إلى مطوّر عقاري يمكنك إضافة عقاراتك بكل سهولة والتعديل علي تفاصيلها مثل السعر والحالة .. إلخ في أي وقت. كما تقوم باستقبال الحجوزات والتأكيد عليها وتعديلها وحذفها متى شئت.</p>
                             <hr>
                           </div>
                         </div>
