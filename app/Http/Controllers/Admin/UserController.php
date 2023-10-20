1a41850f0b01977ae80b5f2ca70273e945c0ce47
sukna-eg <sukna-eg@gmail.com>
2023-10-20 05:54:37 -0700

    fixed

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -1203,6 +1203,7 @@ class UserController extends Controller
 
                         }
 
+                        $paid= $mpaid;
                         $total = $total * 3;
 
                     }
